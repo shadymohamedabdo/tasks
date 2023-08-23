75ac850f797f663e6b7489f9ed40578f463bc926
Shady Mohamed <142973431+shadymohamedabdo@users.noreply.github.com>
2023-08-23 10:47:27 +0300

    Update task2.dart

--- task2.dart
+++ task2.dart
@@ -1,3 +1,16 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'package:hossam/task2.dart' as hossam;
+void main() {
+  ///  task 2 (Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user)
+
+  stdout.write("Hi, please choose a number: ");
+  String? x = stdin.readLineSync();
+  int number = int.parse(x!);
+  if (number % 2 == 0) {
+    print("Chosen number is even");
+  }
+  else {
+    print("Chosen number is odd");
+  }
 }
+
